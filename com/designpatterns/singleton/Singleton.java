6b4d09ae9d890bc78e36fda8fc843dedca2b3189
Kumar Gaurav <krgaurav22@gmail.com>
2018-01-17 00:39:03 +0530

    Added Double Lock

--- com/designpatterns/singleton/Singleton.java
+++ com/designpatterns/singleton/Singleton.java
@@ -5,14 +5,21 @@ package com.designpatterns.singleton;
  */
 public class Singleton {
 
-    private static Singleton instance;
+    /*Without volatile modifier it's possible for another thread in Java to see half initialized state of _instance variable, 
+    but with volatile variable guaranteeing happens-before relationship, 
+    all the write will happen on volatile _instance before any read of _instance variable. */
+    private volatile static Singleton instance;
 
     private Singleton() {
     }
 
     public static synchronized Singleton getInstance() {
         if (instance == null) {
-            instance = new Singleton();
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
         }
         return instance;
     }
